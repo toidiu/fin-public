aacba9658787e469571b723ca1892d9093c43b6d
toidiu <apoorv@toidiu.com>
2018-09-01 03:38:56 -0400

    working non trivial vue app

---
+++ website/src/App.vue
@@ -0,0 +1,32 @@
+import Vue from "vue";
+
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+     Name: <input v-model="name" type="text">
+     <hello-component :name="name" :initialEnthusiasm="5" />
+  </div>
+</template>
+
+<script>
+import HelloComponent from "./components/Hello.vue";
+
+export default {
+  name: 'app',
+  components: {
+    HelloComponent
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App!',
+      name: "World"
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  #app {
+    color: #56b983;
+  }
+</style>
