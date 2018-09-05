df531f044befc48339b204b00c8d1ae77c61fc5f
toidiu <apoorv@toidiu.com>
2018-09-05 00:25:02 -0400

    add ticker and portfolio models to rust

--- fin/src/main.rs
+++ fin/src/main.rs
@@ -7,27 +7,32 @@ extern crate serde_derive;
 
 use rocket::http::Method;
 use rocket_cors::{AllowedOrigins, AllowedHeaders};
+use crate::ticker::*;
 
 
 #[get("/")]
 fn index() -> String {
-    let ticker = models::Ticker {
-        name: "vti".to_owned(),
+    let ticker = Ticker {
+        symbol: TickerSymbol("vti".to_owned()),
         fee: 0.3,
-        current_goal: 20,
-        current_percent: 55
+        price: 150.0,
+        investment_kind: InvestmentKind::Stock,
+        description: "".to_owned(),
+        // current_goal: 20,
+        // current_percent: 55
     };
-    let ticker1 = models::Ticker {
-        name: "voe".to_owned(),
+    let ticker1 = Ticker {
+        symbol: TickerSymbol("voe".to_owned()),
         fee: 0.5,
-        current_goal: 9,
-        current_percent: 5
+        price: 150.0,
+        investment_kind: InvestmentKind::Stock,
+        description: "".to_owned(),
+        // current_goal: 9,
+        // current_percent: 5
     };
 
     let ticker_list = vec![ticker, ticker1];
     serde_json::to_string(&ticker_list).unwrap()
-
-    // "[{ \"name\": \"vti\", \"fee\": \".03\", \"current_goal\": 33, \"current_percent\": 33 }]"
 }
 
 fn main() {
@@ -51,16 +56,81 @@ fn main() {
 
 }
 
-mod models {
+mod ticker {
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct TickerSymbol(pub String);
 
     #[derive(Serialize, Deserialize, Debug)]
     pub struct Ticker {
-        pub name: String,
+        pub symbol: TickerSymbol,
         pub fee: f32,
-        #[serde(rename = "currentGoal")]
-        pub current_goal: u32,
-        #[serde(rename = "currentPercent")]
-        pub current_percent: u32,
+        pub price: f32,
+        pub investment_kind: InvestmentKind,
+        pub description: String,
     }
 
+    #[derive(Serialize, Deserialize, Debug)]
+    pub enum InvestmentKind {
+        Stock,
+        Bond,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct TickerGoal {
+        pub symbol: TickerSymbol,
+        #[serde(rename = "goalPercent")]
+        pub goal_percent: f32,
+        #[serde(rename = "currentPercent")]
+        pub current_percent: f32,
+        #[serde(rename = "currentValue")]
+        pub current_value: f32,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct TickerStatus {
+        ticker: Ticker,
+        goal: TickerGoal,
+    }
+
+}
+
+mod portfolio {
+    use crate::ticker::*;
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Portfolio {
+        pub name: String,
+        pub started: u32,
+        pub current_detail: PortfolioDetails,
+        pub past_detail: Vec<PortfolioDetails>,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct PortfolioDetails {
+        pub tickers: Vec<TickerStatus>,
+        pub goal_stock_percent: f32,
+        pub current_stock_percent: f32,
+        pub deviation_percent: f32,
+    }
+
+}
+
+mod action {
+    use crate::ticker::*;
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub enum ActionType {
+        Buy,
+        Hold,
+        Sell,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Action {
+        symbol: TickerSymbol,
+        action_type: ActionType,
+        date: u32,
+        quantity: u32,
+    }
 }
