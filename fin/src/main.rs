881b89c8ed5ddd848d9f2a046797eb1da414c500
toidiu <apoorv@toidiu.com>
2018-09-08 17:22:39 -0400

    mess around with rust models

--- fin/src/main.rs
+++ fin/src/main.rs
@@ -49,7 +49,7 @@ mod data {
                 description: "des".to_owned(),
             },
             goal: TickerGoal {
-                symbol: TickerSymbol("vti".to_owned()),
+                // symbol: TickerSymbol("vti".to_owned()),
                 goal_percent: 20.0,
                 current_percent: 22.56,
                 current_value: 300.0,
@@ -66,7 +66,7 @@ mod data {
                 description: "des".to_owned(),
             },
             goal: TickerGoal {
-                symbol: TickerSymbol("vtv".to_owned()),
+                // symbol: TickerSymbol("vtv".to_owned()),
                 goal_percent: 6.0,
                 current_percent: 8.35,
                 current_value: 111.0,
@@ -83,7 +83,7 @@ mod data {
                 description: "des".to_owned(),
             },
             goal: TickerGoal {
-                symbol: TickerSymbol("voe".to_owned()),
+                // symbol: TickerSymbol("voe".to_owned()),
                 goal_percent: 4.0,
                 current_percent: 8.65,
                 current_value: 115.0,
@@ -100,7 +100,7 @@ mod data {
                 description: "des".to_owned(),
             },
             goal: TickerGoal {
-                symbol: TickerSymbol("vbr".to_owned()),
+                // symbol: TickerSymbol("vbr".to_owned()),
                 goal_percent: 3.0,
                 current_percent: 10.68,
                 current_value: 142.0,
@@ -117,7 +117,7 @@ mod data {
                 description: "des".to_owned(),
             },
             goal: TickerGoal {
-                symbol: TickerSymbol("vea".to_owned()),
+                // symbol: TickerSymbol("vea".to_owned()),
                 goal_percent: 15.0,
                 current_percent: 9.70,
                 current_value: 129.0,
@@ -134,7 +134,7 @@ mod data {
                 description: "des".to_owned(),
             },
             goal: TickerGoal {
-                symbol: TickerSymbol("vwo".to_owned()),
+                // symbol: TickerSymbol("vwo".to_owned()),
                 goal_percent: 10.0,
                 current_percent: 6.47,
                 current_value: 86.0,
@@ -151,7 +151,7 @@ mod data {
                 description: "des".to_owned(),
             },
             goal: TickerGoal {
-                symbol: TickerSymbol("vtip".to_owned()),
+                // symbol: TickerSymbol("vtip".to_owned()),
                 goal_percent: 3.0,
                 current_percent: 3.68,
                 current_value: 49.0,
@@ -168,7 +168,7 @@ mod data {
                 description: "des".to_owned(),
             },
             goal: TickerGoal {
-                symbol: TickerSymbol("agg".to_owned()),
+                // symbol: TickerSymbol("agg".to_owned()),
                 goal_percent: 4.0,
                 current_percent: 7.97,
                 current_value: 106.0,
@@ -185,7 +185,7 @@ mod data {
                 description: "des".to_owned(),
             },
             goal: TickerGoal {
-                symbol: TickerSymbol("mub".to_owned()),
+                // symbol: TickerSymbol("mub".to_owned()),
                 goal_percent: 14.0,
                 current_percent: 8.2,
                 current_value: 109.0,
@@ -202,7 +202,7 @@ mod data {
                 description: "des".to_owned(),
             },
             goal: TickerGoal {
-                symbol: TickerSymbol("bndx".to_owned()),
+                // symbol: TickerSymbol("bndx".to_owned()),
                 goal_percent: 12.0,
                 current_percent: 8.12,
                 current_value: 108.0,
@@ -219,7 +219,7 @@ mod data {
                 description: "des".to_owned(),
             },
             goal: TickerGoal {
-                symbol: TickerSymbol("vwob".to_owned()),
+                // symbol: TickerSymbol("vwob".to_owned()),
                 goal_percent: 9.0,
                 current_percent: 5.64,
                 current_value: 75.0,
@@ -227,25 +227,24 @@ mod data {
             },
         };
 
+        let detail = PortfolioDetails::new(
+            vec![tg_vti, tg_vtv, tg_voe, tg_vbr, tg_vea, tg_vwo],
+            vec![tg_vtip, tg_agg, tg_mub, tg_bndx, tg_vwob],
+            58.0,
+            5.0,
+        );
+
+        println!("=========");
+        println!("total stocks: {}", detail.get_stock_value());
+        println!("total bonds: {}", detail.get_bond_value());
         let port = Portfolio {
             name: "my portfolio".to_owned(),
             started: 123,
-            current_detail: PortfolioDetails {
-                stocks: vec![tg_vti, tg_vtv, tg_voe, tg_vbr, tg_vea, tg_vwo],
-                bonds: vec![tg_vtip, tg_agg, tg_mub, tg_bndx, tg_vwob],
-                goal_stock_percent: 58.0,
-                current_stock_percent: 66.39,
-                deviation_percent: 5.0,
-            },
+            current_detail: detail,
             past_detail: vec![],
         };
 
         port
-
-        // vec![
-        //     tg_vti, tg_vtv, tg_voe, tg_vbr, tg_vea, tg_vwo, tg_vtip, tg_agg, tg_mub, tg_bndx,
-        //     tg_vwob,
-        // ]
     }
 
 }
@@ -254,6 +253,14 @@ mod ticker {
 
     #[derive(Serialize, Deserialize, Debug, Default)]
     pub struct TickerSymbol(pub String);
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub enum InvestmentKind {
+        #[serde(rename = "stock")]
+        Stock,
+        #[serde(rename = "bond")]
+        Bond,
+    }
 
     #[derive(Serialize, Deserialize, Debug)]
     pub struct Ticker {
@@ -265,24 +272,17 @@ mod ticker {
     }
 
     #[derive(Serialize, Deserialize, Debug)]
-    pub enum InvestmentKind {
-        #[serde(rename = "stock")]
-        Stock,
-        #[serde(rename = "bond")]
-        Bond,
-    }
-
-    #[derive(Serialize, Deserialize, Debug)]
     pub struct TickerGoal {
-        #[serde(skip)]
-        pub symbol: TickerSymbol,
+        // #[serde(skip)]
+        // pub symbol: TickerSymbol,
         #[serde(rename = "goalPercent")]
         pub goal_percent: f32,
+
+        // TODO calculate this
         #[serde(rename = "currentPercent")]
         pub current_percent: f32,
         #[serde(rename = "currentValue")]
         pub current_value: f32,
-
 
         // either keep this here or maybe have another table that records
         // the transactions we have made. we can then calculate what the
@@ -318,27 +318,76 @@ mod portfolio {
         pub stocks: Vec<TickerStatus>,
         pub bonds: Vec<TickerStatus>,
         pub goal_stock_percent: f32,
+
+        // calculated
+        pub total_value: f32,
+        // calculated
         pub current_stock_percent: f32,
         pub deviation_percent: f32,
     }
 
-}
+    impl PortfolioDetails {
+        pub fn new(
+            stocks: Vec<TickerStatus>,
+            bonds: Vec<TickerStatus>,
+            goal_stock_percent: f32,
+            deviation_percent: f32,
+        ) -> Self {
+            PortfolioDetails {
+                stocks: stocks,
+                bonds: bonds,
+                goal_stock_percent: goal_stock_percent,
+                current_stock_percent: 0.0,
+                total_value: 0.0,
+                deviation_percent: deviation_percent,
+            }
+            // calculate total value before others
+            .set_total_value()
+            .set_stock_percent()
+        }
 
-mod action {
-    use crate::ticker::*;
+        pub fn get_stock_value(&self) -> f32 {
+            let s: f32 = self.stocks.iter().map(|s| s.goal.current_value).sum();
+            s.into()
+        }
 
-    #[derive(Serialize, Deserialize, Debug)]
-    pub enum ActionType {
-        Buy,
-        Hold,
-        Sell,
+        pub fn get_bond_value(&self) -> f32 {
+            let b: f32 = self.bonds.iter().map(|s| s.goal.current_value).sum();
+            b.into()
+        }
+
+        fn set_stock_percent(mut self) -> Self {
+            let stocks = self.get_stock_value();
+            self.current_stock_percent = (stocks / self.total_value) * 100.0;
+            self
+        }
+
+        fn set_total_value(mut self) -> Self {
+            let s: f32 = self.stocks.iter().map(|s| s.goal.current_value).sum();
+            let b: f32 = self.bonds.iter().map(|s| s.goal.current_value).sum();
+
+            self.total_value = (s + b).into();
+            self
+        }
     }
 
-    #[derive(Serialize, Deserialize, Debug)]
-    pub struct Action {
-        symbol: TickerSymbol,
-        action_type: ActionType,
-        date: u32,
-        quantity: u32,
-    }
 }
+
+// mod action {
+//     use crate::ticker::*;
+
+//     #[derive(Serialize, Deserialize, Debug)]
+//     pub enum ActionType {
+//         Buy,
+//         Hold,
+//         Sell,
+//     }
+
+//     #[derive(Serialize, Deserialize, Debug)]
+//     pub struct Action {
+//         symbol: TickerSymbol,
+//         action_type: ActionType,
+//         date: u32,
+//         quantity: u32,
+//     }
+// }
