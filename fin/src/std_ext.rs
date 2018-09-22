78a0951bbda7c4c47a0b8e17a8272ee52fe85ac0
toidiu <apoorv@toidiu.com>
2018-09-22 17:21:13 -0400

    fix bug with std_ext and document

--- fin/src/std_ext.rs
+++ fin/src/std_ext.rs
@@ -36,8 +36,24 @@ pub trait ExtIterator: Iterator
 where
     Self: Sized,
 {
+
+    /// This extends the `is_empty()` semantic to Iterator
+    /// trait, which lets us check if the iterator is empty
+    /// without actually creating a collection.
+    ///
+    /// Example:
+    ///
+    /// ```
+    ///     let v = vec![1, 2, 3];
+    ///     let check = v.iter()
+    ///         .filter(|x| **x == 1)
+    ///         // .collect::<Vec<_>>()  // <-- not needed
+    ///         .is_empty();
+    ///     assert_eq!(check, false);
+    /// ```
+    // fixme test!!
     fn is_empty(self) -> bool {
-        self.peekable().peek().is_some()
+        self.peekable().peek().is_none()
     }
 }
 
