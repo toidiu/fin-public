70e410bf930bf3a4c23c5bb6781bc0c1f825143d
toidiu <apoorv@toidiu.com>
2018-11-06 21:16:34 -0500

    fix iex

--- service/iex-rs/src/lib.rs
+++ service/iex-rs/src/lib.rs
@@ -2,12 +2,9 @@
 
 #[macro_use]
 extern crate serde_derive;
-#[macro_use]
-extern crate log;
 
 use reqwest;
 use std::collections::HashMap;
-use std::time::Instant;
 
 pub struct Iex {}
 
@@ -20,9 +17,7 @@ impl Iex {
 
         let uri = format!("https://api.iextrading.com/1.0/stock/market/batch?symbols={}&types=price", s);
 
-        let now = Instant::now();
         let body = reqwest::get(uri.as_str()).unwrap().text().unwrap();
-        debug!("iex request took: {}", now.elapsed().as_millis());
 
         let ret: IexTickersPrice = serde_json::from_str(&body).unwrap();
 
